47a9d6ede0ffcb0d344a5bb9b76468cda58fdb51
yutotakahashi <takahashi.y1022@gmail.com>
2018-06-23 15:57:53 +0900

    本番環境変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,17 @@
 Rails.application.configure do
+
+  config.action_mailer.default_url_options = { host: 'quiet-beach-31655' }
+   ActionMailer::Base.delivery_method = :smtp
+   ActionMailer::Base.smtp_settings = {
+     user_name: ENV['SENDGRID_USERNAME'],
+     password: ENV['SENDGRID_PASSWORD'],
+     domain: "heroku.com",
+     address: "smtp.SendGrid.net",
+     port: 587,
+     authentication: :plain,
+     enable_starttls_auto: true
+   }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.

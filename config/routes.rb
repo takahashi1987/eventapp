4fb50b353c4a753de91d25b2714402491b70d24a
yutotakahashi <takahashi.y1022@gmail.com>
2018-06-23 15:00:54 +0900

    トップページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'tops/index'
+
   resources :events do
     collection do
       post :confirm
     end
   end
+  root 'tops#index'
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
   resources :participant_managements, only: [:create, :destroy]
